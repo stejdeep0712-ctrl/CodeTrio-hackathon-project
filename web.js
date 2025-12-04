d012da88dcde1b92e845f61462bc710ea38c92e1
stejdeep0712-ctrl <stejdeep0712@gmail.com>
2025-12-04 14:55:33 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBilLEzO3FS6xmFRQmkLzuP2GIZUydWhTw",
+    authDomain: "cafe-web-page.firebaseapp.com",
+    databaseURL: "https://cafe-web-page-default-rtdb.firebaseio.com",
+    projectId: "cafe-web-page",
+    storageBucket: "cafe-web-page.firebasestorage.app",
+    messagingSenderId: "403937298847",
+    appId: "1:403937298847:web:79eb724f29a1c8ff1d15aa",
+    measurementId: "G-EET0S21NCG"
+  };
